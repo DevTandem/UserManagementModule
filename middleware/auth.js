7a614ead751e453d16d20f6aa046bdb8e91a34b0
AbhiUd <abhishekudaiyar464@gmail.com>
2024-12-19 20:05:07 +0530

    Token payload

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,9 +2,12 @@ const jwt = require("jsonwebtoken")
 const {secret, reset_secret} = require("../config/jwtConfig")
 
 const auth_middleware = async(req,res,next) => {
-    var token = req.params.token
+    var token
 
-    if(!token) {
+    if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+        token = req.headers.authorization.split(' ')[1];
+    }
+    else {
         return res.status(404).json({message : "Access denied , no token provided"})
     }
 
@@ -20,12 +23,15 @@ const auth_middleware = async(req,res,next) => {
 }
 
 const forgot_pass_middlware = async (req,res,next) => {
-    var token = req.params.token
+    var token
 
-    if(!token) {
+    if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+        token = req.headers.authorization.split(' ')[1];
+    }
+    else {
         return res.status(404).json({message : "Access denied , no token provided"})
     }
-
+    
     try {
         console.log("token",token)
         const decoded = jwt.verify(token, reset_secret)
